cb45fc6b2c5b3b0863876cab998c917ce4560fd8
Akchiche-Mohamed-Aymen <akchiche.mohamedaymen@gmail.com>
2025-04-21 23:07:32 +0100

    add delete

--- dist/home.js
+++ dist/home.js
@@ -2,27 +2,24 @@ const todolist = document.getElementById('todo-list')
 const user =JSON.parse(localStorage.getItem('user'))
 const addBtn = document.querySelector('.add')
 const newTodoInput = document.querySelector('input')
-addBtn.onclick = ()=>alert(5)
-newTodoInput.onchange = ()=>{
-    const text = newTodoInput.value
-    console.log(text)
-    if(text.length > 0){
-        addBtn.disabled = false
-        addBtn.style.background = ''
-  }
-  else{
-    addBtn.disabled = true
-        addBtn.style.background = 'gray'
-  }
-
-}
 addBtn.disabled = true
 addBtn.style.background = 'gray'
-addBtn.onclick = ()=>alert(5)
 document.getElementById('user-name').innerHTML = user.username
-const titles = ['abc' , 'cdv']
-function buildUI(){
-    for(let i = 0 ; i < 2 ; i++){
+const titles = []
+buildUI(titles)
+newTodoInput.addEventListener('input', () => {
+    if (newTodoInput.value.length > 0) {
+        addBtn.disabled = false
+        addBtn.style.background = ''
+    } else {
+        addBtn.disabled = true
+        addBtn.style.background = 'gray'
+    }
+})
+
+function buildUI(titles){
+    todolist.innerHTML = ''
+    for(let i = 0 ; i < titles.length ; i++){
         const todo = `<div class="todo-item p-4  flex items-center" id=${i}>
         <label class="custom-checkbox flex items-center">
             <input type="checkbox" class="todo-checkbox" onclick = 'update(${i})'>
@@ -33,7 +30,7 @@ function buildUI(){
             <button class="edit-todo-btn w-8 h-8 flex items-center justify-center text-gray-500 hover:text-primary">
                 <i class="ri-pencil-line"></i>
             </button>
-            <button class="delete-todo-btn w-8 h-8 flex items-center justify-center text-gray-500 hover:text-red-500">
+            <button class="delete-todo-btn w-8 h-8 flex items-center justify-center text-gray-500 hover:text-red-500" onclick='delete(${i})'>
                 <i class="ri-delete-bin-line"></i>
             </button>
         </div>
@@ -43,9 +40,16 @@ function buildUI(){
     
     }
 }
+addBtn.onclick = ()=>{
+  const newTodoText = newTodoInput.value
+  titles.push(newTodoText)
+  buildUI(titles)
+  newTodoInput.value = ''
+}
 
 
 function update(id) {
     const todo = document.getElementById(`${id}`)
-    console.log(todo)
+    todo.classList.toggle('completed')
 }
+fudelete
