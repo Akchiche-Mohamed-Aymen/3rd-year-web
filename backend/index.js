dc9ad62c2cb272433aef2c1adbc326f327d37b0e
Akchiche-Mohamed-Aymen <akchiche.mohamedaymen@gmail.com>
2025-04-23 19:21:39 +0100

    finish server side

--- backend/index.js
+++ backend/index.js
@@ -1,87 +1,135 @@
 // Import required modules
-import express from 'express'
-import cors from "cors"
-
+import express from "express";
+import cors from "cors";
+import connection from "./connectToDb.js";
 // Initialize express app
 const app = express();
 const PORT = 3000;
-
 // Middleware
 app.use(cors()); // Allow cross-origin requests
 app.use(express.json()); // Parse incoming JSON requests
 
 // Test route
-const users = []
-let todos = []
-app.post('/login', (req, res) => {
-    const {password , username} = req.body
-    const user = users.find(u => u.username === username && u.password === password)
-    if(!user ){
-        return res.status(404).json({msg : "user not exists"})
+const users = [];
+let todos = [];
+app.post("/login", async (req, res) => {
+  const { password, username } = req.body;
+  try {
+    const [[user]] = await connection.query(
+      "select * from users where username = ? AND password = ?",
+      [username, password]
+    );
+    if (!user) {
+      return res.status(404).json({ msg: "user not exists" });
     }
-    res.status(201).json({user})
-
+    res.status(201).json({ user });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 });
 
 // Example POST route
-app.post('/register', (req, res) => {
-    const {password , username} = req.body
-    const find = users.find(u => u.username === username)
-    
-    if(find){
-        return res.status(404).json({msg : "user exists"})
-    }
-    const newUser = {...req.body , id : users.length + 1}
-    users.push(newUser)
-    res.status(201).json({newUser})
+app.post("/register", async (req, res) => {
+  const { password, username } = req.body;
+  try {
+    const result = await connection.query(
+      "insert into users(username , password) values(?,?)",
+      [username, password]
+    );
+    console.log(result);
+    const [[user]] = await connection.query(
+      "select * from users where userId = ? ",
+      [result[0].insertId]
+    );
+    console.log(user);
+    res.status(201).json({ status: 201, user });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 });
 // Example POST route
-app.post('/todos', (req, res) => {
-    const {userId} = req.body
-    const find = users.find(u => u.id === userId)
-    if(!find){
-        return res.status(402).json({msg : "user not exists"})
+app.post("/todos", async (req, res) => {
+  const { userId, title } = req.body;
+  try {
+    if (!userId || !title) {
+      return res.status(401).json({ msg: "invalid credentials" });
     }
-    const newTodo = {...req.body , id : todos.length + 1 , completed : false}
-    todos.push(newTodo)
-   res.status(201).json({newTodo})
+    const [[find]] = await connection.query(
+      "select * from users where userId = ? ",
+      [userId]
+    );
+    if (!find) {
+      return res.status(401).json({ msg: "user not exists" });
+    }
+    const result = await connection.query(
+      "insert into todos(title , userId) values(?,?)",
+      [title, userId]
+    );
+    const [[newTodo]] = await connection.query(
+      "select * from todos where id = ?",
+      [result[0].insertId]
+    );
+    res.status(201).json({ newTodo });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 });
-app.put('/todos/:id', (req, res) => {
-    const {id} = req.params
-    let  todo = todos.find(u => u.id == id)
-    if(!todo){
-        return res.status(404).json({msg : "todo not exists"})
+app.put("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, completed } = req.body;
+  try {
+    const [[todo]] = await connection.query(
+      "select * from todos where id = ? ",
+      [id]
+    );
+    if (!todo) {
+      return res.status(404).json({ msg: "todo not exists" });
     }
-    todo = {...todo , ...req.body}
-    todos = todos.map(t=>{
-        if(t.id != id) return t
-        else return todo
-    })
-    res.status(201).json({todo , status: 200})
+    let result = null;
+    if (title && title.length > 0) {
+      result = await connection.query(
+        "update todos set title = ? where id = ?",
+        [title, id]
+      );
+      console.log(result);
+    }
+    if (completed != undefined) {
+      result = await connection.query(
+        "update todos set completed = ? where id = ?",
+        [completed, id]
+      );
+      console.log(result);
+    }
+    res.status(201).json({ msg: "todo updated" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 });
-app.get('/todos/:id', (req, res) => {
-    const {id} = req.params
-    console.log(id)
-    let  exist = todos.find(u => u.userId == id)
-    if(!exist){
-        return res.status(404).json({msg : "todos not exists"})
-    }
-    let todoOwn = todos.filter(t => id == t.userId)
-    res.status(201).json({ todos : todoOwn , status: 200})
+app.get("/todos/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const [todos] = await connection.query(
+      "select * from todos where userId = ? ",
+      [userId]
+    );
+
+    return res.status(200).json({ todos });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 });
-app.delete('/todos/:id', (req, res) => {
-    const {id} = req.params
-    console.log(req.body)
-    const {userId} = req.body
-    let todo = todos.find(u => u.userId === +userId)
-    console.log(todo , userId , id)
-    if(!todo){
-        return res.status(404).json({msg : "todos not exists"})
-    }
-    todos = todos.filter(t => +id !== t.id)
-    for(let i = 0 ; i < todos.length ; i++)
-        todos[i].id = i+1
-    res.status(201).json({ todos , status: 200})
+app.delete("/todos/:id", async(req, res) => {
+  const { id } = req.params;
+  const { userId } = req.body;
+ try {
+    const result = await connection.query("delete from todos where id =  ? and userId = ?" , [id , userId])
+    res.status(202).json({ msg: "todo deleted" });
+
+ } catch (err) {
+    res.status(500).json({ msg: err.message });
+
+ }
 });
 
 // Start server
